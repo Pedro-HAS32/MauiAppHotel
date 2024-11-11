2d069d36a77b5733cc6b17feb59e47bd5687ddc5
Pedro-HAS32 <pedrohas12@hotmail.com>
2024-11-11 00:25:09 -0300

    Termino Agenda 14

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,41 @@
-﻿
+﻿using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        //O <Quarto> significa que o código está usando o modelo criado na pasta "Models".
+        //O código na primeira linha diz para o aplicativo usar o modelo "Quarto" dentro da pasta models
+        //O código abaixo cria uma nova lista chamada "lista_quartos" que usa o modelo <Quarto>.
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Single",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+
+        };
+
         public App()
         {
             InitializeComponent();
@@ -14,7 +47,7 @@ namespace MauiAppHotel
         {
             var window = base.CreateWindow(activationState);
 
-            window.Width = 400; //Largura inícial da janela
+            window.Width = 600; //Largura inícial da janela
             window.Height = 600; //Altura inícial da janela
 
             window.MinimumWidth = 400; //Largura miníma da janela
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,13 +2,56 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+    App PropriedadesApp;
+
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+        //Descobrir o que o código abaixo faz
+        PropriedadesApp = (App)Application.Current;
+
+        //pck_quarto é o nome dado ao picker de data no ContratacaoHospedam.xaml
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        //Define uma data minima e uma data máxima para ser escolhida
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+
+    }
 
     private void btn_sobre_Clicked(object sender, EventArgs e)
     {
 		App.Current.MainPage = new Sobre();
+    }
+
+    // |-----------------> Anotar! <-----------------|
+    // |Fazer nota sobre o funcionamento do Try/Catch|
+    // |---------------------------------------------|
+
+    //Troca de tela com animação
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+
+        } catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddDays(6);
     }
 }
